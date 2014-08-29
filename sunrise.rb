b9affde5c0b3d1af989f68558bd397e4a703d0a0
William Pietri <william@scissor.com>
2014-08-29 15:58:03 -0700

    changing dawn to end with warm light, and only get more blue later in the day

--- sunrise.rb
+++ sunrise.rb
@@ -3,7 +3,9 @@
 require 'json'
 require 'rest_client'
 
-pace=60 # seconds per minute
+sunrise_time = ARGV[0] || 30
+pace = sunrise_time * 2 # seconds per minute, basically
+
 BASE_URL = "http://192.168.1.199/api/080ed655b6f74144a29fd2f256eff3ae"
 
 def group(number,state)
@@ -17,13 +19,13 @@ end
 # sunrise comes up over 30 minutes to full brightness
 
 # 0-5 bring light 1 up
-light 1, {on: true, bri: 0, xy:[0.65,0.32], transitiontime: 0}
+light 1, {bri: 0, xy:[0.65,0.32], transitiontime: 0, on: true}
 sleep 0.1
 light 1, {bri: 50, xy:[0.62,0.35], transitiontime: 5*pace*10}
 sleep 5*pace
 
 # 5-10 bring light 2 up; shift light 1 color
-light 2, {on: true, bri: 0, xy:[0.62,0.35], transitiontime: 0}
+light 2, {bri: 0, xy:[0.62,0.35], transitiontime: 0, on:true}
 sleep 0.1
 light 1, {bri: 50, xy:[0.58,0.39], transitiontime: 5*pace*10}
 light 2, {bri: 50, xy:[0.58,0.39], transitiontime: 5*pace*10}
@@ -31,11 +33,16 @@ sleep 5*pace
 #now lights are in sync, so use groups
 
 # 10-20 shift to warm light at medium brightness
-
-group 0, {bri: 125, ct:350, transitiontime: 10*pace*10}
+group 0, {bri: 125, ct:500, transitiontime: 10*pace*10}
 sleep 10*pace
 
-# 20-30 shift to cool light at high brightness
-group 0, {bri: 255, ct:153, transitiontime: 10*pace*10}
+# 20-30 shift to normal light at high brightness
+group 0, {bri: 255, ct:333, transitiontime: 10*pace*10}
 sleep 10*pace
+
+# over the next few hours, get daylight-y
+group 0, {ct:153, transitiontime: 180*pace*10}
+
+
+
 
