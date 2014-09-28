a3dbd8410617ab93a463b6a487d1ce853e4f80a3
William Pietri <william@scissor.com>
2014-09-28 12:40:44 -0700

    Extracting duplication. Adding a DaylightLumensCurve.

---
+++ src/main/scala/DaylightCurve.scala
@@ -0,0 +1,18 @@
+import org.joda.time.LocalTime
+
+trait DaylightCurve {
+  val MillisPerDay = 24 * 60 * 60 * 1000
+
+  val interpolator: breeze.interpolation.UnivariateInterpolator[Double]
+
+  def timeAsFractionOfDay(t: LocalTime): Double = {
+    1.0 * t.millisOfDay().get() / MillisPerDay
+  }
+
+  def t(hours: Int, minutes: Int = 0): Double = (60 * hours + minutes) / (60.0 * 24)
+
+  def apply(t: LocalTime): Double = {
+    interpolator(timeAsFractionOfDay(t))
+  }
+
+}
--- src/main/scala/DaylightLumensCurve.scala
+++ src/main/scala/DaylightLumensCurve.scala
@@ -2,21 +2,31 @@ import breeze.interpolation._
 import breeze.linalg.DenseVector
 import org.joda.time.LocalTime
 
-object DaylightColorCurve extends DaylightCurve {
+/*
+Hue bulb ratings
 
+550 lm @ 6500K
+600 lm @ 4000K
+510 lm @ 3000K
+360 lm @ 2000K
+*/
+
+/** Maps time to desired lumen levels */
+object DaylightLumensCurve extends DaylightCurve {
   val interpolator = new LinearInterpolator(
     DenseVector(Array(
       t(0), t(6, 15), t(7), t(9),
       t(12),
       t(16), t(19), t(20, 30), t(23, 59))),
     DenseVector(Array[Double](
-      1000, 1000, 3000, 6500,
-      20000,
-      6500, 3000, 1000, 1000))
+      0, 1, 2000, 5000,
+      10000,
+      5000, 2500, 1, 0))
   )
 
-  override def apply(t: LocalTime): Double = {
+  override def apply(t: LocalTime) = {
     interpolator(timeAsFractionOfDay(t))
   }
+
 
 }
