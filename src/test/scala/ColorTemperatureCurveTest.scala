66dcb02970aa3ef6c0098a40051aea2b971788e7
William Pietri <william@scissor.com>
2014-10-02 16:13:18 -0700

    Made the ColorTemperature API match the other curves

--- src/test/scala/ColorTemperatureCurveTest.scala
+++ src/test/scala/ColorTemperatureCurveTest.scala
@@ -4,29 +4,25 @@ import org.scalatest.matchers.ShouldMatchers
 class ColorTemperatureCurveTest extends FlatSpec with ShouldMatchers {
 
   "Warm end" should "be correct" in {
-    val c = new ColorTemperatureCurve()
-    val (x, y) = c(1000)
+    val (x, y) = ColorTemperatureCurve(1000)
     x should be(0.653 plusOrMinus 0.005)
     y should be(0.342 plusOrMinus 0.005)
   }
 
   "Mid-warm" should "be correct" in {
-    val c = new ColorTemperatureCurve()
-    val (x, y) = c(1900)
+    val (x, y) = ColorTemperatureCurve(1900)
     x should be(0.538 plusOrMinus 0.005)
     y should be(0.411 plusOrMinus 0.005)
   }
 
   "Middle" should "be correct" in {
-    val c = new ColorTemperatureCurve()
-    val (x, y) = c(3600)
+    val (x, y) = ColorTemperatureCurve(3600)
     x should be(0.400 plusOrMinus 0.005)
     y should be(0.387 plusOrMinus 0.005)
   }
 
   "Mid-cold" should "be correct" in {
-    val c = new ColorTemperatureCurve()
-    val (x, y) = c(6500)
+    val (x, y) = ColorTemperatureCurve(6500)
     x should be(0.314 plusOrMinus 0.005)
     y should be(0.313 plusOrMinus 0.005)
   }
@@ -34,8 +30,7 @@ class ColorTemperatureCurveTest extends FlatSpec with ShouldMatchers {
 
 
   "Cold end" should "be correct" in {
-    val c = new ColorTemperatureCurve()
-    val (x, y) = c(20000)
+    val (x, y) = ColorTemperatureCurve(20000)
     x should be(0.258 plusOrMinus 0.005)
     y should be(0.258 plusOrMinus 0.005)
   }
